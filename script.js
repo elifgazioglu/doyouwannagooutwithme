931ead5962b6ee6b343496b04ef12dec3dee22c2
Dory <dorym1995@hotmail.com>
2023-12-23 21:47:25 +1000

    Updated and organised code

---
+++ script.js
@@ -0,0 +1,11 @@
+function showYesPage() {
+    document.getElementById('page1').style.display = 'none';
+    document.getElementById('page2').style.display = 'block';
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth);
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight);
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
